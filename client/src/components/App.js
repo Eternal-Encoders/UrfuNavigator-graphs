0cafcb51dc154cdddae08de3f55f32158e14b741
Roaoch <103529423+Roaoch@users.noreply.github.com>
2022-12-03 01:56:59 +0500

    Minor Update

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Point from "./GraphPoint"
 
 const Indoor = require('indoorjs')
 
@@ -7,6 +8,8 @@ class App extends React.Component {
         super(props)
         this.mapImg = props.mapImg
         this.map = undefined;
+        this.coords = [];
+        this.afterClick = this.afterClick.bind(this);
     }
 
     componentDidMount() {
@@ -23,15 +26,39 @@ class App extends React.Component {
             maxZoom: 100,
             center: {
               x: 0,
-              y: 0,
-              zoom: 10
+              y: 0
             }
         })
     }
 
+    afterClick(e) {
+        const xCoord = (e.clientX - 10 + this.map.originX) / this.map.zoom + this.map.center.x
+        const yCoord = (e.clientY - 10 + this.map.originY) / this.map.zoom - this.map.center.y
+        let key = e.clientX + e.clientY
+        const pointToPush = new Point({
+            key: key, 
+            id: key, 
+            x: xCoord, 
+            y: yCoord
+        });
+        this.coords.push(pointToPush);
+
+        const marker = new Indoor.Marker([xCoord, yCoord], {
+            text: `${this.coords.length}`,
+            draggable: true,
+            zIndex: 100,
+            id: pointToPush.id
+        });
+        marker.addTo(this.map)
+        console.log(this.map)
+    }
+
     render() {
         return (
-            <div ref={(el) => this._canv = el}>
+            <div 
+                ref={(el) => this._canv = el}
+                onClick={this.afterClick}
+            >
             </div>
         )
     }
