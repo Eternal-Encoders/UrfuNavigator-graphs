5e99bc3f3fbd79368af92a8cb8e716ec8c0b90e7
Roaoch <103529423+Roaoch@users.noreply.github.com>
2023-08-03 17:56:07 +0500

    Update FormStairId.tsx

--- src/components/form-components/form-stair-id/FormStairId.tsx
+++ src/components/form-components/form-stair-id/FormStairId.tsx
@@ -8,16 +8,18 @@ import {
 import "./form-stair-id-style.css";
 
 interface FormStairIdProps {
-    stairId: string
+    stairId: string,
+    setStairId: (stairId: string) => void
 }
 
-function FormStairId({stairId}: FormStairIdProps) {
+function FormStairId({stairId, setStairId}: FormStairIdProps) {
     return (
         <FormGroup>
             <FormLabel>Id лестницы</FormLabel>
             <FormControl 
                 type="text" 
                 value={stairId}
+                onChange={(e) => setStairId(e.target.value)}
             />
         </FormGroup>
     );
